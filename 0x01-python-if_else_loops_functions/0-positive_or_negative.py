4dee89673d36640a1d2858c27f7aae18bceafbe1
gpradinett <gpradinett@gmail.com>
2022-04-26 14:00:49 -0500

    syntax error

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,8 +2,8 @@
 import random
 number = random.randint(-10, 10)
 if number > 0:
-    print("is positive")
+    print(f"{number} is positive")
 elif number == 0:
-    print("iz zero")
-elif number < 0:
-    print("is negative")
+    print(f"{number} is zero")
+else:    
+    print(f"{number} is negative")

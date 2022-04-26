2c5e6e2f8adc6e6930e35cd44b53f51dd260a36c
gpradinett <gpradinett@gmail.com>
2022-04-26 16:38:57 -0500

    syntax error

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if i == '101' or i == '113':
+    if i == 101 or i == 113:
         continue
-    print(f"{chr(i)}", end='')
+    else:
+        print(f"{chr(i)}", end='')

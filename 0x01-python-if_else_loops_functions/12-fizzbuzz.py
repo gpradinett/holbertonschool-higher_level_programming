25f70fd235b7ee5a2c372bdfd074c3da0774a2b1
gpradinett <gpradinett@gmail.com>
2022-04-26 19:20:43 -0500

    syntax error

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,8 +4,8 @@ def fizzbuzz():
         if i % 3 == 0 and i % 5 == 0:
             print("fizzbuzz", end=' ')
         elif i % 3 == 0:
-            print("Fizz", end='')
+            print("Fizz", end=' ')
         elif i % 5 == 0:
-            print("Buzz", end='')
+            print("Buzz", end=' ')
         else:
             print(i, end=' ')

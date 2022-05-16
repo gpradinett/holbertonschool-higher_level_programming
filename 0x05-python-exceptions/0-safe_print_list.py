6fb31f2b55dffb835399749ec4c1f7b433301427
gpradinett <gpradinett@gmail.com>
2022-05-16 17:50:37 -0500

    Python - Exceptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    while i < x:
+        try:
+            print(f"{my_list[i]}", end="")
+        except Exception:
+            break
+        i += 1
+    print("")
+    return i

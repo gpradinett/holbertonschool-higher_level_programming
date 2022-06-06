8cdea070d0d5b085c65578ea3e2a2a5badc2eb99
gpradinett <gpradinett@gmail.com>
2022-06-06 16:47:25 -0500

    fixing __str__ error

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -31,7 +31,8 @@ class Square(Rectangle):
 
     def __str__(self):
         """Returns a string representation of a Square instance"""
-        s = "[Square], ({}) {}/{} - {}".format(
+        s = "[{}], ({}) {}/{} - {}".format(
+                self.__class__.__name__,
             self.id, self.x, self.y, self.width)
 
         return s

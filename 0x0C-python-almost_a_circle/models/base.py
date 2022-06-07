720ad18ba487832522c903775034ef92606713f2
gpradinett <gpradinett@gmail.com>
2022-06-06 20:31:52 -0500

    corrigiendo error de diccionario

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,9 +28,35 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """
+        Returns a JSON representation of list_dictionaries.
+        Args:
+            - list_dictionaries: list of dicts
+        Returns: JSON representation of the list
 
-        """
-        if list_dictionaries is None or list_dictionaries == 0:
-            return []
+        
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            list_dictionaries = []
 
         return json.dumps(list_dictionaries)
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        
+        if (type(list_dictionaries) != list or
+           not all(type(x) == dict for x in list_dictionaries)):
+            raise TypeError("list_dictionaries must be a list of dictionaries")
+        return json.dumps(list_dictionaries)
+    
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+
+        """
+        if list_objs is None or list_objs == []:
+            jstr = "[]"
+        else:
+            jstr = cls.to_json_string([o.to_dictionary() for o in list_objs])
+            filename = cls.__name__ + ".json"
+        with open(filename, 'w') as f:
+                f.write(jstr)
+        

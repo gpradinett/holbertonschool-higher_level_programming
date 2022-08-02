508a4d59455b9f97dda61ddbdf7ad1b718f4b719
gpradinett <gpradinett@gmail.com>
2022-08-02 01:32:11 -0500

    ✅ changing let variable to const

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-// varibale print a message JavaScript is amazing
+// variable print a message JavaScript is amazing
 
-const myVar = "JavaScript is amazing";
+const myVar = 'JavaScript is amazing';
 
 console.log(myVar);

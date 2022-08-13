0c5b228a471fbf30b2bb1eb066b008caf0ee165a
gpradinett <gpradinett@gmail.com>
2022-08-12 23:51:54 -0500

    ✅ initial commit

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -5,26 +5,22 @@ with the name passed as argument
  from the database hbtn_0e_6_usa
 """
 
-if __name__ == "__main__":
-
-    from sys import argv
-    from model_state import Base, State
+if __name__ == '__main__':
+    import sys
     from sqlalchemy import create_engine
+    from model_state import Base, State
     from sqlalchemy.orm import sessionmaker
-
     if len(sys.argv) == 5:
         engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format
                                (sys.argv[1], sys.argv[2], sys.argv[3]))
-
-    Base.metadata.create_all(engine)
-
-    Session = sessionmaker(bind=engine)
-    flag = 1
-    conn = Session()
-    for state in conn.query(State).order_by(State.id).all():
-        if sys.argv[4] == state.name:
-            flag = 0
-            print("{}".format(state.id))
-    if flag == 1:
-        print("Not found")
-    conn.close()
+        Base.metadata.create_all(engine)
+        Session = sessionmaker(bind=engine)
+        flag = 1
+        conn = Session()
+        for state in conn.query(State).order_by(State.id).all():
+            if sys.argv[4] == state.name:
+                flag = 0
+                print("{}".format(state.id))
+        if flag == 1:
+            print("Not found")
+        conn.close()

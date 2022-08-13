420fc7e48e57292e8e92c13e7538c834b70f7d4b
gpradinett <gpradinett@gmail.com>
2022-08-12 23:38:11 -0500

    ✅ fix error pycode

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Write a script that lists all State objects that contain the letter a from the database hbtn_0e_6_usa
+"""lists all State objects contain the letter a from the database hbtn_0e_6_usa
 """
 
 if __name__ == '__main__':
 
-	import sys
-	from sqlalchemy import create_engine
-	from model_state import Base, State
-	from sqlalchemy.orm import Session
+    import sys
+    from sqlalchemy import create_engine
+    from model_state import Base, State
+    from sqlalchemy.orm import Session
 
-	engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(argv[1], argv[2], argv[3]))
 
     Base.metadata.create_all(engine)

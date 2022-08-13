20f02c33f9ee97c699aa7d8d1201b679466aaba8
gpradinett <gpradinett@gmail.com>
2022-08-12 23:40:13 -0500

    ✅ fix error pycode

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -9,8 +9,9 @@ if __name__ == '__main__':
     from model_state import Base, State
     from sqlalchemy.orm import Session
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(argv[1], argv[2], argv[3]))
+    if len(sys.argv) == 4:
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format
+                               (sys.argv[1], sys.argv[2], sys.argv[3]))
 
     Base.metadata.create_all(engine)
 

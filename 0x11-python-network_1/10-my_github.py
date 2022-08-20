de7ee61db8cc16b83b291e92355f28d29db648cf
gpradinett <gpradinett@gmail.com>
2022-08-19 23:49:51 -0500

    ✅ initial commit

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-script that takes your GitHub credentials (username and password) and uses the GitHub APIto display your id
+script that takes your GitHub credentials
+(username and password) and uses the GitHub APIto display your id
 https://blog.devgenius.io/how-to-use-api-request-in-python-6ef370f9f771
 """
 
@@ -9,8 +10,8 @@ from requests.auth import HTTPBasicAuth
 import sys
 
 if __name__ == "__main__":
-    user = sys.argv[1] # se guarda el usuario pasado como primer comando en linea
-    passwd = sys.argv[2] # guardamos la contr que se pase como 2 comand en linea
+    user = sys.argv[1]  # se guarda usuario pasado como argv[3]
+    passwd = sys.argv[2]  # se guarda pass se pase como argv[2]
     url = "https://api.github.com/user"
     # Se hace una request usando autentificacion
     response = get(url, auth=HTTPBasicAuth(user, passwd)).json()

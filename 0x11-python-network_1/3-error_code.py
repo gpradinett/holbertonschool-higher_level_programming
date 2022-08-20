bc76070cd1f9571eb9c0e882feaba0b56ff21e33
gpradinett <gpradinett@gmail.com>
2022-08-19 22:53:36 -0500

    ✅ initial commit

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     import urllib.request
     from sys import argv
 
-    url = urllib.request.Request(argv[1])
+    req = urllib.request.Request(argv[1])
     try:
         with urllib.request.urlopen(req) as response:
             print(response.read().decode("UTF-8"))

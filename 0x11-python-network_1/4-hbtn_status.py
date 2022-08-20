cfa3de9e4532d9727e170685e0c5be9276d3f709
gpradinett <gpradinett@gmail.com>
2022-08-19 22:57:19 -0500

    ✅ initial commit

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Python script fetches https://intranet.hbtn.io/status
+from requests import get
+
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+    response = get(url).text
+    print("Body response:\n\t- type: {}\n\t- content: {}"
+          .format(type(response), response))

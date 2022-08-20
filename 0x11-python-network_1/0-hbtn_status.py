ef245dc880a6749609fa4814dd7d1898e37b5bc9
gpradinett <gpradinett@gmail.com>
2022-08-19 20:43:54 -0500

    ✅ initial commit

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Fetches https://intranet.hbtn.io/status
+"""
+if __name__ == "__main__":
+    import urllib.request
+
+    url = "https://intranet.hbtn.io/status"
+    request = urllib.request.Request(url)
+
+    with urllib.request.urlopen(request) as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type:", type(content))
+        print("\t content:", content)
+        print("\t utf conten:", content.decode("utf-8"))

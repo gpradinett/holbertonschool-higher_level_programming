1bb71f104a2d8052f7b8c241c3d16399234e8cf8
gpradinett <gpradinett@gmail.com>
2022-09-13 17:21:06 -0500

    ✅ initial commit

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,19 +1,19 @@
 #!/usr/bin/node
 /*
-Write a script that reads and prints the content of a file.
-
-The first argument is the file path
-The content of the file must be read in utf-8
-If an error occurred during the reading, print the error object
-process.argv --> https://www.geeksforgeeks.org/node-js-process-argv-propert    y/
+Write a script that writes a string to a file:
+- The first argument is the file path
+- The second argument is the string to write
+- The content of the file must be written in utf-8
+- If an error occurred during while writing, print the error object
+Info write a file: https://nodejs.dev/en/learn/writing-files-with-nodejs/
 */
 
 // Include process module
 const process = require('process');
 const args = process.argv; // I define args as process.argv
 
-const path = args[2];
-const texto = args[3];
+const path = args[2]; // file where the content will be written
+const texto = args[3]; // content to write
 const fs = require('fs'); // load fs class
 
 fs.writeFile(path, texto, 'utf-8', (err) => {

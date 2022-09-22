c52e79f0ab1c90d20f691fe19a344aa3e8ee4c84
gpradinett <gpradinett@gmail.com>
2022-09-22 00:53:53 -0500

    ✅ add task 8

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,11 @@
+// Write a JavaScript script that fetches and lists the title for all movies by using this URL: https://swapi-api.hbtn.io/api/films/?format=json
+
+// Write a JavaScript script that updates the text of the <header> element to New Header!!! when the user clicks on DIV#update_header
+// GET jquery: https://jquery-tutorial.net/ajax/the-get-and-post-methods/
+const $ = window.$;
+
+$.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
+  $.each(data.results, function (index, element) {
+    $('UL#list_movies').append('<li>' + element.title + '</li>');
+  });
+});

79734e4d6ff9e72c8c901bdd7d1479c51abe006e
gpradinett <gpradinett@gmail.com>
2022-09-21 22:53:23 -0500

    ✅ add task 3

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,11 @@
+// Write a JavaScript script that adds the class red to the <header> element when the user clicks on the tag DIV#red_header
+// tips: https://api.jquery.com/addClass/
+
+const $ = window.$;
+
+$(document).ready(function () {
+  const button = $('DIV#red_header');
+  button.click(function () {
+    $('header').addClass('red');
+  });
+});

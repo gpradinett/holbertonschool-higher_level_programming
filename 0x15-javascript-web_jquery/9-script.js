dd0700f63d5ed5b0516417141e4c62d1e6141b4f
gpradinett <gpradinett@gmail.com>
2022-09-22 01:03:43 -0500

    ✅ add task 9

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,9 @@
+// Write a JavaScript script that fetches from https://fourtonfish.com/hellosalut/?lang=fr and displays the value of hello from that fetch in the HTML tag DIV#hello.
+// Lo que tenia de complicado esta tarea es que habia que ver hacia donde te rediriga la pagina
+// inicial que era (https://fourtonfish.com/hellosalut/?lang=fr), pasamos un curl -k y con esto nos trae la pagina de la reedireccion
+// Defino $ para que no haya problemas con el semistandard
+const $ = window.$;
+
+$.get('https://stefanbohacek.com/hellosalut/?lang=fr', function (data) {
+  $('DIV#hello').text(data.hello);
+});

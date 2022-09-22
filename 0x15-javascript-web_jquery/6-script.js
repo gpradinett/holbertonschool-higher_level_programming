fba62c5e9346f9ed71a91ab9f44d54f0403abb0d
gpradinett <gpradinett@gmail.com>
2022-09-22 00:03:26 -0500

    ✅ add task 6

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,10 @@
+// Write a JavaScript script that updates the text of the <header> element to New Header!!! when the user clicks on DIV#update_header
+// tips: https://api.jquery.com/text/
+const $ = window.$;
+
+$(document).ready(function () {
+  const button = $('DIV#update_header');
+  button.click(function () {
+    $('header').text('New Header!!!');
+  });
+});

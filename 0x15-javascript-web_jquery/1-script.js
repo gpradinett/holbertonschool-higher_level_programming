24bbbe697b5513701effbde4f21276e920523b1c
gpradinett <gpradinett@gmail.com>
2022-09-21 21:51:39 -0500

    ✅ add task 1

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,7 @@
+// Write a JavaScript script that updates the text color of the <header> element to red (#FF0000):
+// Definition JQuery: https://www.w3schools.com/jquery/jquery_syntax.asp
+// propiedad window: https://developer.mozilla.org/en-US/docs/Web/API/Window/window
+// Defino $ para que no haya problemas con el semistandard
+const $ = window.$;
+
+$('header').css('color', '#FF0000');

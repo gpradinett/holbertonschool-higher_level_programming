ab3dd3dd1a4282ed50307fbb0e7ca16fa04e8e11
gpradinett <gpradinett@gmail.com>
2022-09-21 20:40:09 -0500

    ✅ add js task 0 change color text to red

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,4 @@
+// Write a JavaScript script that updates the text color of the <header> element to red (#FF0000)
+// Info JavaScript selector: https://www.w3schools.com/jsref/met_document_queryselector.asp
+
+document.querySelector('header').style.color = "#ff0000";

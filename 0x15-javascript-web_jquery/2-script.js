8db22e5dd0aad41a2b763f4260822f11ec2d3df0
gpradinett <gpradinett@gmail.com>
2022-09-21 22:12:08 -0500

    ✅ add task 2

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,11 @@
+// Write a JavaScript script that updates the text color of the <header> element to red (#FF0000) when the user clicks on the tag DIV#red_header:
+// tips: https://stackoverflow.com/questions/2941679/change-text-color-using-jquery
+// Defino $ para que no haya problemas con el semistandard
+const $ = window.$;
+
+$(document).ready(function () {
+  const button = $('#red_header');
+  button.click(function () {
+    button.css('color', '#FF0000');
+  });
+});

53003c6edbc0fa705977e2b3d6097367ed0aebf2
gpradinett <gpradinett@gmail.com>
2022-09-21 23:22:27 -0500

    ✅ add task 4

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,11 @@
+// Write a JavaScript script that toggles the class of the <header> element when the user clicks on the tag DIV#toggle_header:
+// Info Toggle classes: https://www.w3schools.com/jquery/html_toggleclass.asp
+// "Toggle" significa "alternar"
+const $ = window.$;
+
+$(document).ready(function () {
+  const button = $('DIV#toggle_header');
+  button.click(function () {
+    $('header').toggleClass('red green');
+  });
+});

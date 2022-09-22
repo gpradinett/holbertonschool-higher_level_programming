16628a824ac0966f8df7b581086b36570f56d075
gpradinett <gpradinett@gmail.com>
2022-09-21 23:45:33 -0500

    ✅ add task 5

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,10 @@
+// tips: https://api.jquery.com/append/
+// Write a JavaScript script that adds a <li> element to a list when the user clicks on the tag DIV#add_item:
+const $ = window.$;
+
+$(document).ready(function () {
+  const button = $('DIV#add_item');
+  button.click(function () {
+    $('ul').append('<li>Item</li>');
+  });
+});

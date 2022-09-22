6a6ac42c0247a32debadacc4e095bb0ec877476e
gpradinett <gpradinett@gmail.com>
2022-09-22 00:32:47 -0500

    ✅ add task 7

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,9 @@
+// Write a JavaScript script that updates the text of the <header> element to New Header!!! when the user clicks on DIV#update_header
+// GET jquery: https://jquery-tutorial.net/ajax/the-get-and-post-methods/
+const $ = window.$;
+
+$.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function (data, status) {
+  // el ID "character"
+  $('#character').text(data.name);
+}
+);

038c9cd7c630ec2d29235376d24ab37f9a0889df
gpradinett <gpradinett@gmail.com>
2022-09-23 00:05:56 -0500

    ✅ add task 100

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,7 @@
+// Write a JavaScript script that updates the text color of the <header> element to red (#FF0000):
+// Info addEventListener: https://www.w3schools.com/js/js_htmldom_eventlistener.asp
+// DOMcontentloaded --> https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('header').style.color = '#FF0000';
+});
